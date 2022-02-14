a93123a1b26d9a2f56c8b0ae59b58717b6a058d1
kishanfowler <kv18226@ijburgcollege.nl>
2022-02-14 10:50:01 +0100

    les 1 brons af

---
+++ les01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace les01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tijd = 0;
+            Console.WriteLine(tijd);
+            System.Threading.Thread.Sleep(1000);
+            while (tijd != -1) {
+                System.Threading.Thread.Sleep(1000);
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+                Console.WriteLine("Het is nu {0}", time);
+            }
+        }
+    }
+}
